2ef8a942ab06880262c4876c9a6b20f90a4ccb04
Ivan <ivan@massvision.net>
2019-02-27 22:01:42 +0100

    Initial commit

---
+++ scripts.js
@@ -0,0 +1,25 @@
+var request = new XMLHttpRequest();
+
+request.open('GET', 'https://api.openbrewerydb.org/breweries', true);
+request.onload = function () {
+
+  // Begin accessing JSON data here
+  var data = JSON.parse(this.response);
+
+  if (request.status >= 200 && request.status < 400) {
+    data.forEach(beer => {
+      //console.log(beer.name + " - " + beer.city);
+      document.getElementById("root").innerHTML += 
+      "<div class='beer_card'>" +
+        "<div class='name'><a target='_blank' href='" + beer.website_url + "'>" + beer.name + "</a></div>" + 
+        "<div class='brewery_type " + beer.brewery_type + "'>" + beer.brewery_type + "</div>" +
+        "<div class='street'>" + beer.street + "</div>" +
+        "<div class='city'>" + beer.city + "</div>" +
+      "</div>";
+    });
+  } else {
+    console.log('error');
+  }
+}
+
+request.send();
